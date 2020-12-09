8ce8b3a54e274f9cde87956a35e36bb89d750e8c
shannu <yourshannu1@gmail.com>
2020-12-09 19:11:24 +0530

    JPQL + NATIVE QUERY + @Param

--- Sapient_Test_Product_Catalog_Search/src/main/java/com/xyz/ecommerce/repository/ProductRepository.java
+++ Sapient_Test_Product_Catalog_Search/src/main/java/com/xyz/ecommerce/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.xyz.ecommerce.model.Product;
@@ -20,8 +21,11 @@ public interface ProductRepository extends CrudRepository<Product, Integer>{
 	List<Product> findBySize(int size);
 	
 	//@Query(value = "select ud from product ud where ud.price between ?1 and ?2")//JPQL
-	@Query(value = "select * from Product where price between ?1 and ?2", nativeQuery=true)
-	List<Product> getProductByPrices(Double priceRange1, Double priceRange2);
+	//@Query(value = "select * from Product where price between ?1 and ?2", nativeQuery=true)
+	//List<Product> getProductByPrices(Double priceRange1, Double priceRange2);
+	
+	@Query(value = "select * from Product where price between :priceRange1 and :priceRange2", nativeQuery=true)
+	List<Product> getProductByPrices(@Param("priceRange1")  Double priceRange1,@Param("priceRange2") Double priceRange2);
 	
 
 }
